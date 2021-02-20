5bbd1c4da41716a60257e169b9ac89b2b7cdddd2
doylet <thomas.doyle84@gmail.com>
2021-02-20 20:11:01 +1000

    fixed migration

--- db/migrate/20210220100740_create_posts.rb
+++ db/migrate/20210220100740_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :subject
       t.text :body
-      t.int :user_id
+      t.integer :user_id
 
       t.timestamps
     end
